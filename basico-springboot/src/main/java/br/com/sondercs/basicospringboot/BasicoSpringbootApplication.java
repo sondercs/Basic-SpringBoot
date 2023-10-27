47d6561a278d5d7618d69526813a3780b8bf1bae
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-27 00:08:58 +0000

    Criado o projeto ultilizando o Spring Initializr

---
+++ basico-springboot/src/main/java/br/com/sondercs/basicospringboot/BasicoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.sondercs.basicospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicoSpringbootApplication.class, args);
+	}
+
+}
---
+++ basico-springboot/src/test/java/br/com/sondercs/basicospringboot/BasicoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sondercs.basicospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
